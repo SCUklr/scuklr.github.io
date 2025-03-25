62ffe256c97ef1f7b1d293d052c7dfea1293ccbc
SCUklr <SCUklr@users.noreply.github.com>
2025-03-25 13:43:33 +0000

    deploy: e7747132a8846295f125d2de35cba8523aa2bcff

---
+++ assets/naive-ui-beautify-Dl6EpJ_R.js
@@ -0,0 +1,20 @@
+const n=`---
+title: 使用Naive UI美化界面
+date: 2024-03-20
+tags: ['Naive UI', 'UI设计']
+description: 如何使用 Naive UI 组件库来美化你的 Vue 应用...
+---
+
+# 使用Naive UI美化界面
+
+## 引言
+Naive UI 是一个优雅的 Vue3 组件库...
+
+## 正文
+1. 安装 Naive UI
+2. 配置主题
+3. 使用组件
+4. 自定义样式
+
+## 总结
+Naive UI 提供了丰富的组件和主题定制能力... `;export{n as default};
