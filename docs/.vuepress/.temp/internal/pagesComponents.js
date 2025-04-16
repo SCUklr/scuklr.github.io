207e3dfb8cf18499622c2b033f61dc99187ad848
SCU-Ziqiu <3761297418@qq.com>
2025-04-16 13:58:40 +0800

    回归原样

---
+++ docs/.vuepress/.temp/internal/pagesComponents.js
@@ -0,0 +1,18 @@
+import { defineAsyncComponent } from 'vue'
+
+export const pagesComponents = {
+  // path: /system-design.html
+  "v-38e3bc14": defineAsyncComponent(() => import(/* webpackChunkName: "v-38e3bc14" */"D:/Documents/大学/个人项目/Ziqiu_Blog_Project/frontend/docs/.vuepress/.temp/pages/system-design.html.vue")),
+  // path: /system-design.html
+  "v-38e3bc14": defineAsyncComponent(() => import(/* webpackChunkName: "v-38e3bc14" */"D:/Documents/大学/个人项目/Ziqiu_Blog_Project/frontend/docs/.vuepress/.temp/pages/system-design.html.vue")),
+  // path: /404.html
+  "v-3706649a": defineAsyncComponent(() => import(/* webpackChunkName: "v-3706649a" */"D:/Documents/大学/个人项目/Ziqiu_Blog_Project/frontend/docs/.vuepress/.temp/pages/404.html.vue")),
+  // path: /posts/
+  "v-e1e3da16": defineAsyncComponent(() => import(/* webpackChunkName: "v-e1e3da16" */"D:/Documents/大学/个人项目/Ziqiu_Blog_Project/frontend/docs/.vuepress/.temp/pages/posts/index.html.vue")),
+  // path: /posts/2025/04/CypressABC.html
+  "v-5df37d1c": defineAsyncComponent(() => import(/* webpackChunkName: "v-5df37d1c" */"D:/Documents/大学/个人项目/Ziqiu_Blog_Project/frontend/docs/.vuepress/.temp/pages/posts/2025/04/CypressABC.html.vue")),
+  // path: /posts/2025/04/How%20to%20deploy%20my%20personal%20blog%20project%20on%20GitHub%20to%20your%20local.html
+  "v-26562690": defineAsyncComponent(() => import(/* webpackChunkName: "v-26562690" */"D:/Documents/大学/个人项目/Ziqiu_Blog_Project/frontend/docs/.vuepress/.temp/pages/posts/2025/04/How to deploy my personal blog project on GitHub to your local.html.vue")),
+  // path: /posts/2025/03/future-outlook.html
+  "v-494156fa": defineAsyncComponent(() => import(/* webpackChunkName: "v-494156fa" */"D:/Documents/大学/个人项目/Ziqiu_Blog_Project/frontend/docs/.vuepress/.temp/pages/posts/2025/03/future-outlook.html.vue")),
+}
---
+++ docs/.vuepress/.temp/internal/pagesRoutes.js
@@ -0,0 +1,5 @@
+export const pagesRoutes = [
+  ["v-38e3bc14","/system-design.html",{"title":"个人博客系统概要设计文档"},[":md"]],
+  ["v-3706649a","/404.html",{"title":""},[]],
+  ["v-26562690","/posts/2025/04/How%20to%20deploy%20my%20personal%20blog%20project%20on%20GitHub%20to%20your%20local.html",{"title":"Github入门——以把本博客部署到你的本地为例"},["/posts/2025/04/How to deploy my personal blog project on GitHub to your local.html","/posts/2025/04/How to deploy my personal blog project on GitHub to your local.md",":md"]],
+]
